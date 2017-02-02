a2ec2453c1c0971d9ce57514b63a5f7adb99d493
John Kim <yohany@gmail.com>
2017-02-02 14:16:39 +0900

    init project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# redmine-dashboard-template
-Starter project for Reamine API and customized UI Server 
+# REDMINE DASHBOARD (Template for development)
+
+* API tunneling (API proxy)
+* handlebars UI pages
+
+
+### Run Server
+
+```shell
+$ REDMINE_HOST=http://redmine_url
+$ REDMINE_APIKEY=API_KEY
+$ npm start
+```
+
+> You can find API KEY at the left side on http://redmine_url/my/account
+
+
+
+### References
+
+Redmine API http://www.redmine.org/projects/redmine/wiki/Rest_api
+
---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var hbs = require('express-hbs');
+var fs = require('fs');
+var fp = require('path');
+var Redmine = require('node-redmine');
+
+var relative = (path) => fp.join(__dirname, path);
+
+console.log(process.env.REDMINE_HOST, { apiKey: process.env.REDMINE_APIKEY });
+
+var redmine = new Redmine(process.env.REDMINE_HOST, { apiKey: process.env.REDMINE_APIKEY });
+
+var redmineApi = (resource, parameter, callback) => {
+  console.log(resource, parameter);
+  redmine[resource](parameter, (err, data) => {
+    console.log(err, data);
+    if (err) callback(err);
+    callback(data);
+  });
+};
+
+var app = express();
+app.use(express.static(relative('public')));
+app.engine('hbs', hbs.express4({
+  partialsDir: [relative('views/partials')]
+}));
+app.set('view engine', 'hbs');
+app.set('views', relative('views'));
+
+// API request mapper
+app.get('/api/:resource', function(req, res) {
+  var result = redmineApi(req.params.resource, req.query, (result) => res.json(result) );
+});
+
+// Page request mapper
+app.get('/', (req, res) => {
+  res.render('index', {
+    // put parameters using for hbs templates
+  });
+});
+
+app.listen(process.env.PORT || 9000);
+// Add more page reqeust mappers
+// . . . . . .
